366fa1a73d3c43e100627358a9864a2f1f86ac25
Frosco03 <jcejercito@up.edu.ph>
2024-01-07 23:17:10 +0800

    started conversion and did the start of the queue logic

--- conversion.asm
+++ conversion.asm
@@ -2,5 +2,26 @@
 	InPostConversion:
 		#Joenne: Check if the value of array is an operator and add to stack accordingly 
 		#James: Check if the value of array is a number and add to queue accordingly
+		
+		#Check if the current array value is a number
+		#Assume that $t1 = member of the array
+		li  $t0, '0'
+		bltu   $t1,$t0, notdig        # Jump if char < '0'
+
+		li $t0,'9'
+		bltu   $t0,$t1, notdig       # Jump if '9' < char
+		
+		#Value is a digit
+		dig:
+			#If digit, move the number to the output queue	
+			
+			b end
+			
+		#Value is not a digit
+		notdig:
+		
+		end:
 
 .data
+
+.include "queue.asm"
