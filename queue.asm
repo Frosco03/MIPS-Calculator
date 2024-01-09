9278b156aa05d523d01d42457d37d97749d2d09d
Frosco03 <jcejercito@up.edu.ph>
2024-01-10 00:10:09 +0800

    added proper enqueue operation

--- queue.asm
+++ queue.asm
@@ -1,18 +1,26 @@
 .text
-	li $t0, 5	#counter = number of members of the array
-	la $t1, array
+	queue_contstruct:
+		li $t0, 100	#counter = number of members of the array
+		la $t1, array
 	
-	input_loop:
-		beqz $t0, end_input
-	
-		la $a0, prompt
-		jal PromptInt
+	enqueue:
+		#store the return address 
+		addi $sp, $sp, -4
+		sw $ra, 0($sp)
 		
-		sw $v0, 0($t1)
+		beqz $t0, end_input #skip the queue if array is full
+	
+		#Value that will be stored in the array is at $a0
+		sw $a0, 0($t1)
 		addi $t1, $t1, 4
 		
-		addi $t0, $t0, -1
-		b input_loop
+		addi $t0, $t0, -1 #decrement counter
+		
+		#Get the return address and return the memory
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		
+		jr $ra
 	
 	end_input:
 		li $t0, 5	#counter
