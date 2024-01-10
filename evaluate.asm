8e9056497caf0da4c4fa5eb8b25bea7e5b01266d
jamie <91231086+jm-amancio@users.noreply.github.com>
2024-01-10 11:54:22 +0800

    replace temp reg with float reg; add stack operations

--- evaluate.asm
+++ evaluate.asm
@@ -34,53 +34,80 @@
 		# Else if value is not a digit, pop OperandStack twice and 
 		#	use the operator to perform operation on the two popped elements
 		notdig:
-			# num1 = $s6, num2 = $s7
-			# pop off the top element $s7 of the OperandStack
+			# num1 = $f1, num2 = $f2
+			# pop off the top element of the OperandStack, and load value to a float register
 			lw $t7, OperatorStack_TopIndex	# load current OperandStack_TopIndex
-			lw $s7, OperatorStack($t7)	# save OperandStack top element to $s7, then pop it off
+			l.s $f2, OperatorStack($t7)	# save OperandStack top element to $s7, then pop it off
 			addi $t7, $t7, -4
 			sw $t7, OperatorStack_TopIndex	# update OperatorStack_TopIndex
 			
-			# pop off the top element $s6 of the OperandStack
+			# pop off the top element of the OperandStack, and load value to a float register
 			lw $t7, OperatorStack_TopIndex	# load current OperandStack_TopIndex
-			lw $s6, OperatorStack($t7)	# save OperandStack top element to $s6, then pop it off
+			l.s $f1, OperatorStack($t7)	# save OperandStack top element to $s6, then pop it off
 			addi $t7, $t7, -4
 			sw $t7, OperatorStack_TopIndex	# update OperatorStack_TopIndex
 			
-			##### Convert $s6 and $s7 to floating type
-			
-			# perform operation on $s6 and $s7 based on operator
+			# perform operation on $f1 and $f2 based on operator
 			beq $s0, $t1, doAdd	# if $t1 == + 
 			beq $s1, $t1, doSub	# if $t1 = -
 			beq $s2, $t1, doMul	# if $t1 == * 
 			beq $s3, $t1, doDiv	# if $t1 == /
 			
-			##### CODE BELOW IS THE GIST OF THE IMPLEMENTATION; REPLACE TO cater to floating type
-			
 			doAdd:
-				add $t2, $s6, $s7
-				##### PUSH $t2 TO OperandStack; ADD CODE HERE
-			
+				add.s $f0, $f1, $f2
+				b pushResultToOperandStack
+				
 			doSub:
-				sub $t2, $s6, $s7
-				##### PUSH $t2 TO OperandStack; ADD CODE HERE
+				sub.s $f0, $f1, $f2
+				b pushResultToOperandStack
 			
 			doMul:
-				mul $t2, $s6, $s7
-				##### PUSH $t2 TO OperandStack; ADD CODE HERE
+				mul.s $f0, $f1, $f2
+				b pushResultToOperandStack
 			
 			doDiv:
-				div $t2, $s6, $s7
-				##### PUSH $t2 TO OperandStack; ADD CODE HERE
-			
+				div.s $f0, $f1, $f2
+				b pushResultToOperandStack
+				
+			# PUSH $f0 TO OperandStack
+			pushResultToOperandStack:
+				lw $t7, OperandStack_TopIndex	# load current OperatorStack_TopIndex
+				addi $t7, $t7, 4
+				swc1 $f0, OperandStack($t7)
+				sw $t7, OperandStack_TopIndex	# update OperatorStack_TopIndex
+				
+				#####b PostEvaluation	# move to next element of the queue
+				
 		end:
-			jal Exit
+		
+		
+	printResult:
+	# If the end of the PostfixQueue is reached, then the result is top OperatorStack top element;
+	# 	pop off the top element of the OperandStack, and load value to a float register
+		lw $t7, OperandStack_TopIndex	# load current OperandStack_TopIndex
+		l.s $f12, OperandStack($t7)	# save OperandStack top element to $f12, then pop
+		li.s $f13, 0.0        		# set $f13 to 0.0 (least significant half)
+		addi $t7, $t7, -4
+		sw $t7, OperatorStack_TopIndex	# update OperatorStack_TopIndex
+	
+		# Print result text (with $v0 = 4)
+		li $v0, 4
+		la $a0, result
+		syscall
+		
+		# Print result float (with $v0 = 2 and $f12, $f3 loaded above)
+		li $v0, 2
+		syscall
+		
+		jal Exit
 			
 			
 .data
-PostfixQueue: .space 200	# TEMPORARY Array to store the postfix expression
-OperandStack: .space 200	# Array to store the OperandStack elements
-OperandStack_TopIndex: .word 0	# Initialize TopIndex to 0
+	.align 4
+	PostfixQueue: .space 200	# TEMPORARY Array to store the postfix expression
+	OperandStack: .space 200	# Array to store the OperandStack elements
+	OperandStack_TopIndex: .word 0	# Initialize TopIndex to 0
+	result: .asciiz "The result is "
 
 .include "utils.asm"
 .include "queue.asm"
