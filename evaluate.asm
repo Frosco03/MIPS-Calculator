282dae49e98d34053c6dc7886eba9cdf5b34c438
jamie <91231086+jm-amancio@users.noreply.github.com>
2024-01-15 11:47:24 +0800

    clean code comments

--- evaluate.asm
+++ evaluate.asm
@@ -8,45 +8,43 @@
 
 .text
 	PostEvaluation:
-	# Get the output queue array address
+	# Get the output queue (postfix expression) array address at $a0
 	move $t2, $a0
 	li $s0, 0 		# Initialize OperandStack top index to 0	
 	
 	evaluationLoop:
-	lw $t3, 0($t2)		# save the current element of output queue
-	beq $t3, $zero, printResult	# if you are at the end of the output queue, go to end 
-	addi $t2, $t2, 4	# increment so in next iteration, we read next element of array
+	lw $t3, 0($t2)			# save the current element of output queue (postfix expression)
+	beq $t3, $zero, printResult 	# if you are at the end of the output queue, go to printResult
+	addi $t2, $t2, 4		# increment so in next iteration, we read next element of array
 		
-		# If value is not a digit (value < 0)
-		# because + is -57, - is -55, * is -58, / is -53
+		# If value is not a digit (value < 0, because + is -57, - is -55, * is -58, / is -53), go to notdigit
 		bltz $t3, notdigit
 		
-		# If value is a digit
+		# Else if value is a digit, then push $t3 to the OperandStack
 		digit:
-		# then push $t3 to the OperandStack
 		sw $t3, OperandStack($s0)	# store $t3 to OperatorStack
 		addi $s0, $s0, 4		# increment OperatorStack top index
 		j evaluationLoop
 			
-		# Else if value is not a digit, pop OperandStack twice and 
-		#	use the operator to perform operation on the two popped elements
 		notdigit:
+			# pop OperandStack twice, then use the operator to perform operation on them
+		
 			# num1 = $f1, num2 = $f2
 			# pop off the top element of the OperandStack, and load value to a float register
 			addi $s0, $s0, -4
 			lwc1 $f2, OperandStack($s0)	# save OperandStack top element to $f2, then pop it
-			lw $s2, OperandStack($s0)	# save OperandStack top element to $f2, then pop it
+			lw $s2, OperandStack($s0)	# also save number to $s2
 			
 			# pop off the top element of the OperandStack, and load value to a float register
 			addi $s0, $s0, -4
 			lwc1 $f1, OperandStack($s0)	# save OperandStack top element to $f1, then pop it
-			lw $s1, OperandStack($s0)
+			lw $s1, OperandStack($s0)	# also save number to $s1
 			
 			# perform operation on $f1 and $f2 based on operator
-			beq $t1, -57, doAdd	# if $t1 == + 	== (add_op ascii = 43) - 100 == -57
-			beq $t1, -55, doSub	# if $t1 == -	== (sub_op ascii = 45) - 100 == -55
-			beq $t1, -58, doMul	# if $t1 == * 	== (mul_op ascii = 42) - 100 == -58
-			beq $t1, -53, doDiv	# if $t1 == /	== (div_op ascii = 47) - 100 == -53
+			beq $t3, -57, doAdd	# if $t3 == + 	== (add_op ascii = 43) - 100 == -57
+			beq $t3, -55, doSub	# if $t3 == -	== (sub_op ascii = 45) - 100 == -55
+			beq $t3, -58, doMul	# if $t3 == * 	== (mul_op ascii = 42) - 100 == -58
+			beq $t3, -53, doDiv	# if $t3 == /	== (div_op ascii = 47) - 100 == -53
 			
 			doAdd:
 				add.s $f0, $f1, $f2
@@ -57,6 +55,8 @@
 				j pushResultToOperandStack
 			
 			doMul:
+				# perform integer multiplication instead of float multiplication,
+				# because the latter results to 0. but save the result to float $f0
 				mul $s3, $s1, $s2
 				mfhi $s4
 				mtc1 $s3, $f0
@@ -67,43 +67,35 @@
 			
 			doDiv:
 				div.s $f0, $f1, $f2
-				c.eq.s $f0, $f0		# this line activates printNowDontCovert at label printResult
+				c.eq.s $f0, $f0		# this line activates printNowDontCovert when at label printResult
 				j pushResultToOperandStack
 				
 			# PUSH $f0 TO OperandStack
 			pushResultToOperandStack:
-				l.s $f0, OperandStack($s0)	# store $f0 to OperatorStack
+				swc1 $f0, OperandStack($s0)	# store $f0 to OperatorStack
 				addi $s0, $s0, 4		# increment OperatorStack top index
 				j evaluationLoop		# move to next element of the queue
 		
 	# If the end of the PostfixQueue is reached, then the result is top OperatorStack top element;
 	# 	pop off the top element of the OperandStack, and load value to a float register
 	printResult:
-		addi $s0, $s0, -4
-		l.s $f12, OperandStack($s0)	# save OperandStack top element to $f12, then pop it
-		#l.s $f13, 0.0        		# set $f13 to 0.0 (least significant half)
-	
 		# Print result text (with $v0 = 4)
 		li $v0, 4
 		la $a0, result
 		syscall
 		
-<<<<<<< Updated upstream
-=======
 		addi $s0, $s0, -4
 		lw $t5, OperandStack($s0) 	#Save the OperandStack top element to $t5 and pop it
 		mtc1 $t5, $f12 			# move $t5 to $f12
 		bc1t printNowDontCovert	### If division was performed, jump to printNowDontCovert. Else, convert.
 		cvt.s.w $f12, $f12 		# Convert the value of $f12 (still a word value) to a floating-point value
 		
->>>>>>> Stashed changes
 		# Print result float (with $v0 = 2 and $f12, $f3 loaded above)
 		printNowDontCovert:
 		li $v0, 2
 		syscall
 		
 		jr $ra
-			
 			
 .data
 	.align 4
